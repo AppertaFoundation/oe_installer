d42fe50b0dbb1d715ff23a7ff6c0afd810db890f
Antony Penn <antony.penn@gmail.com>
2015-11-25 14:13:13 +0000

    install cleanup

--- install/Gruntfile.js
+++ install/Gruntfile.js
@@ -1,42 +1,63 @@
 module.exports = function(grunt) {
-  grunt.initConfig({
-//    pkg: grunt.file.readJSON('package.json')
-//  },
-    
-  watch: {
-    compass: {
-      files: ['../openeyes/**/*.{scss,sass}'],
-      tasks: ['compass:dev']
-    },
-    js: {
-      files: ['../openeyes/**/*.js'],
-      tasks: ['uglify']
-    }
-  },
+	grunt.initConfig({
  
-  compass: {
-    dev: {
-      options: {              
-	sassDir: '../openeyes/protected/assets/sass',
-        cssDir: '../openeyes/protected/assets/css',
-        imagesDir: '../openeyes/protected/assets/img',
-        generatedImagesDir: '../openeyes/protected/assets/img/sprites',
-        outputStyle: 'expanded',
-        relativeAssets: true,
-        httpPath: '',
-        noLineComments: false,
-        importPath: [
-	    '../openeyes/protected/assets/components/foundation/scss/',
-	    '/var/www/openeyes/protected/assets/components/foundation/scss/'
-        ]
-      }
-    },
-  }}),
-  
-  grunt.loadNpmTasks('grunt-contrib-watch');
-  grunt.loadNpmTasks('grunt-contrib-compass');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+	compass: {
+		dev: {
+			options: {              
+				sassDir: '../openeyes/protected/assets/sass',
+				cssDir: '../openeyes/protected/assets/css',
+				imagesDir: '../openeyes/protected/assets/img',
+				generatedImagesDir: '../openeyes/protected/assets/img/sprites',
+				outputStyle: 'expanded',
+				relativeAssets: true,
+				httpPath: '',
+				noLineComments: false,
+				importPath: [
+					'../openeyes/protected/assets/components/foundation/scss/',
+					'/var/www/openeyes/protected/assets/components/foundation/scss/'
+				]
+			}
+		},
+	}}),
+   
 
-  grunt.registerTask('default', ['compass:dev', 'uglify', 'watch']);
+	grunt.registerTask('buildmodule', function(dir) {
+		var done = this.async();
+
+		grunt.log.writeln('Processing ' + dir);
+
+		grunt.util.spawn({
+			grunt: true,
+			args:['compass'],
+			opts: {
+				cwd: dir
+			}
+		},
+
+		function(err, result, code) {
+			if (err == null) {
+				grunt.log.writeln('processed ' + dir);
+				done();
+			}
+			else {
+				grunt.log.writeln('module ' + dir + ' not found: ' + code);
+				done();	// false for failure/halt
+			}
+		})
+	});
+
+	grunt.registerTask('buildmodules', function() {
+		grunt.task.run(['buildmodule:../openeyes/protected/modules/OphCiExamination']);
+		grunt.task.run(['buildmodule:../openeyes/protected/modules/OphCiPhasing']);
+	});
+
+
+	grunt.loadNpmTasks('grunt-contrib-watch');
+	grunt.loadNpmTasks('grunt-contrib-compass');
+	grunt.loadNpmTasks('grunt-contrib-uglify');
+
+	grunt.registerTask('default', ['compass:dev', 'uglify', 'buildmodules']);
+
+
 };
 
