6368b0a49949dfab5ce0dc121cfec1b74e001f49
Antony Penn <antony.penn@gmail.com>
2015-11-26 12:03:14 +0000

    added chmod +x for run.sh

--- install/install-oe.sh
+++ install/install-oe.sh
@@ -138,6 +138,8 @@ fi
 cp /vagrant/install/dicom-file-watcher.conf /etc/init
 cp /vagrant/install/dicom /etc/cron.d/
 
+chmod +x /var/www/openeyes/protected/cli_commands/file_watcher/run.sh
+
 useradd iolmaster -s /bin/false -m
 mkdir /home/iolmaster/test
 mkdir /home/iolmaster/incoming
