177669c88d934dcaeb4ca78198630c750ffd91ca
Antony Penn <antony.penn@gmail.com>
2015-12-07 13:39:06 +0000

    removed sed of fam.conf as file has been removed

--- install/install-system.sh
+++ install/install-system.sh
@@ -48,7 +48,6 @@ ln -s /usr/bin/nodejs /usr/bin/node
 #  Install pre-compiled FAM module and configure PHP to use it
 sed -i "s/;   extension=msql.so/extension=fam.so/" /etc/php5/apache2/php.ini
 sed -i "s/;   extension=msql.so/extension=fam.so/" /etc/php5/cli/php.ini
-sed -i "s/^local_only = true/local_only = false/" /etc/fam.conf
 cp /vagrant/install/fam.so /usr/lib/php5/20121212/
 
 
