01de8a926ac7c689cca098949ce2f8857c5c4550
Antony Penn <antony.penn@gmail.com>
2015-12-16 14:45:01 +0000

    Added phpunit.phar download to install-devtools

--- install/install-devtools.sh
+++ install/install-devtools.sh
@@ -31,6 +31,13 @@ sudo npm install grunt-contrib-concat --save-dev
 sudo gem install compass --no-ri --no-rdoc
 
 
+# install phpunit (we need version 3.x)
+
+wget https://phar.phpunit.de/phpunit-3.7.38.phar
+chmod +x phpunit-3.7.38.phar
+mv phpunit-3.7.38.phar /usr/local/bin/phpunit
+
+
 echo --------------------------------------------------
 echo DEVTOOLS INSTALLED
 echo Please check previous messages for any errors
