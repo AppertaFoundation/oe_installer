797e179358ba33a743197629044c7c7e02617069
tobyoe <toby.bisco@openeyes.org.uk>
2016-01-28 19:31:03 +0000

    updated installers to ignore core modules

--- install/install-oe.sh
+++ install/install-oe.sh
@@ -16,7 +16,7 @@ cd /var/www
 # TODO: if openeyes dir exists, delete it
 
 
-git clone -b feature/IOLMaster https://github.com/openeyes/OpenEyes.git openeyes 
+git clone -b feature/testrepomerge https://github.com/openeyes/OpenEyes.git openeyes 
 cd openeyes/protected
 unzip yii.zip
 unzip vendors.zip
