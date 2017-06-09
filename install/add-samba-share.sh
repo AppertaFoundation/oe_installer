5d18a84cc3e1aca8c76f4893c5861e41ea5e1014
biskyt <toby.bisco@openeyes.org.uk>
2017-06-09 11:34:53 +0100

    changed samba share to use root

--- install/add-samba-share.sh
+++ install/add-samba-share.sh
@@ -1,15 +1,35 @@
 #!/bin/bash
 
-echo "Configuring SAMBA share 'openeyes'. NOTE: This script should only be run once, otherwise it will mess up your /etc/samba/smb.conf"
+printf "\e[32mConfiguring SAMBA share 'openeyes'. NOTE: This script should only be run once, otherwise it will mess up your /etc/samba/smb.conf\e[0m"
 
 sudo apt update
 sudo apt install samba -y
 
-sudo smbpasswd -a vagrant
+echo -e "vagrant\nvagrant" | sudo smbpasswd -s -a root
 
 echo "
 [openeyes]
-path = /var/www/openeyes
-valid users = vagrant
-read only = no
+        path = /var/www/openeyes
+        valid users = vagrant root
+        admin users = root vagrant
+        write list = root vagrant
+        force user = root
+        force group = root
+        group = root
+        read only = No
+
 " >> /etc/samba/smb.conf
+
+sudo service smbd restart
+sudo testparm -s
+
+printf "\e[32m
+************************************************
+
+Added samba share \\\\\\openeyes.vm\\openeyes
+
+Connect as:-
+User: root
+Password: vagrant
+
+************************************************\e[0m"
