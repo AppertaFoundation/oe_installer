45b5d54ab7277ac5ee4acb3469b9cbbe9f2a6a09
biskyt <toby.bisco@openeyes.org.uk>
2017-08-25 11:30:03 +0100

    Always reset institution

--- install/install-oe.sh
+++ install/install-oe.sh
@@ -364,6 +364,26 @@ if [ ! "$live" = "1" ]; then
 	mysql -u root "-ppassword" < /tmp/openeyes-mysql-setbanner.sql
 	rm /tmp/openeyes-mysql-setbanner.sql
 
+    # Set default institution code
+    icode=$(grep -oP '(?<=institution_code. => .).*?(?=.,)' /var/www/openeyes/protected/config/local/common.php)
+
+    if [ ! -z $icode ]; then
+
+        echo "
+
+        setting institution to $icode
+
+        "
+
+        echo "UPDATE institution SET remote_id = '$icode' WHERE id = 1;" > /tmp/openeyes-mysql-institute.sql
+
+        mysql -uroot "-ppassword" -D openeyes < /tmp/openeyes-mysql-institute.sql
+
+        rm /tmp/openeyes-mysql-institute.sql
+
+
+    fi
+
 fi
 
 
