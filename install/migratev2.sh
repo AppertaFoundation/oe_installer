697332d0293a840538b6a83ca7d3033b0d480828
biskyt <toby.bisco@openeyes.org.uk>
2017-08-25 10:56:02 +0100

    use php in front of yiic commands

--- install/migratev2.sh
+++ install/migratev2.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 printf "\e[31mMIGRATING ALLERGIES TO V2\e[0m\n"
-/var/www/openeyes/protected/yiic allergymigrate
+php /var/www/openeyes/protected/yiic allergymigrate
 printf "\e[31mMIGRATING FAMILY HISTORY TO V2\e[0m\n"
-/var/www/openeyes/protected/yiic familyhistorymigrate
+php /var/www/openeyes/protected/yiic familyhistorymigrate
 printf "\e[31mMIGRATING PAST SURGERY TO V2\e[0m\n"
-/var/www/openeyes/protected/yiic pastsurgerymigrate
+php /var/www/openeyes/protected/yiic pastsurgerymigrate
 printf "\e[31mMIGRATING SOCIAL HISTORY TO V2\e[0m\n"
-/var/www/openeyes/protected/yiic socialhistorymigrate
+php /var/www/openeyes/protected/yiic socialhistorymigrate
 printf "\e[31mMIGRATING RISKS TO V2\e[0m\n"
-/var/www/openeyes/protected/yiic risksmigrate
+php /var/www/openeyes/protected/yiic risksmigrate
 printf "\e[31mMIGRATING BIOMETRY LENSES TO V2\e[0m\n"
-##/var/www/openeyes/protected/yiic biometrymigrate
+##php /var/www/openeyes/protected/yiic biometrymigrate
