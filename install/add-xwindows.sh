0364a2840b1ea5ddb634fa3c134670b815860f60
biskyt <toby.bisco@openeyes.org.uk>
2017-10-05 08:57:40 +0100

    script to optionally install x-windows (xfce)

---
+++ install/add-xwindows.sh
@@ -0,0 +1,16 @@
+sudo add-apt-repository ppa:webupd8team/atom -y
+sudo apt update
+
+
+sudo apt-get install --no-install-recommends xubuntu-desktop -y
+
+
+
+sudo chown -R vagrant:vagrant .
+sudo find /home/vagrant/ -type d -exec chmod 750 {} +
+sudo find /home/vagrant/ -type f -exec chmod 640 {} +
+
+sudo apt install chromium-browser atom -y
+
+echo "You must set a password for the Ubuntu user..."
+sudo passwd ubuntu
