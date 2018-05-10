af3ebf0a4dd93598e9dd03b54e7a400050ea909f
biskyt <github@gettheitguy.com>
2018-05-10 10:25:11 +0100

    attemp to make the same script work for both 14.04 and 16.04

--- install/install-dicom.sh
+++ install/install-dicom.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
 # Copy DICOM related files in place as required
-sudo cp -f /vagrant/install/dicom-file-watcher.service /etc/systemd/system/
- # sudo cp -f /vagrant/install/dicom /etc/cron.d/ # shouldn't be needed
+if [[ `lsb_release -rs` == "14.04" ]]; then
+    # Ubuntu 14.04 uses upstart / init.d
+    sudo cp -f /vagrant/install/dicom-file-watcher.conf /etc/init/
+    sudo cp -f /vagrant/install/dicom /etc/cron.d/
+else
+    # Ubuntu 14.10 and higher uses systemd
+    sudo cp -f /vagrant/install/dicom-file-watcher.service /etc/systemd/system/
+fi
+
 sudo cp -f /vagrant/install/run-dicom-service.sh /usr/local/bin
 sudo chmod +x /usr/local/bin/run-dicom-service.sh
 
