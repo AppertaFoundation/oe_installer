360db4c0cfa878db3e58506553e5d8e53e0391d8
biskyt <github@gettheitguy.com>
2018-05-14 11:50:58 +0100

    fix permission when in live mode

--- install/install-oe.sh
+++ install/install-oe.sh
@@ -309,7 +309,7 @@ if [ $live = 1 ]; then
 # envtype can be one of LIVE, AWS or VAGRANT
 env=LIVE
 envtype=LIVE
-" >/etc/openeyes/env.conf
+" | sudo tee /etc/apache2/sites-available/000-default.conf >/dev/null
 fi
 
 # Copy sample configuration and fix some file permissions
